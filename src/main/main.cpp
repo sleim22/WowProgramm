51e6eb61e61ad67780559f3ed33bd5a5c81c1f77
Lukas Deutscher <L.Deutscher@gmx.net>
2018-07-26 17:46:20 +0200

    UPDATE: Added basic main

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,16 +1,19 @@
+/**
+ * @file
+ * @brief The main file with the main entry point
+ *
+ * @author Lukas Deutscher
+ * @date 26.07.2018
+ */
 
-// The basic input output
-#include <iostream>
-#include <string>
-
-int main()
+/**
+ * @brief The default main function
+ *
+ * @param argc The number of command line arguments
+ * @param argv The command line arguments as an array of c-style strings
+ * @return int The Program exit code
+ */
+int main(int argc, char** argv)
 {
-    std::cout << "Please enter your name: ";
-
-    std::string Name;
-    std::cin >> Name;
-
-    std::cout << "Hello " << Name << std::endl;
-
     return 0;
 }
